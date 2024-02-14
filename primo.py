2b4a3baac9adf1b2f7c7a483b3cbd594b38c4717
lorenzozannini <159887488+lorenzozannini@users.noreply.github.com>
2024-02-14 16:37:15 +0100

    Add files via upload

--- primo.py
+++ primo.py
@@ -1,5 +1,14 @@
-from math import sqrt
+from cmath import asin, cos, sin, sqrt
+from math import radians
 c1=10.123
 c2=30.456
 risultato=sqrt((c1**2)+(c2**2))
 print("ipotenusa=",risultato)
+
+r=6371
+ϕ1=radians(59.9)
+λ1=radians(10.8)
+ϕ2=radians(49.3)
+λ2=-radians(-123.1)
+d=2*r*asin(sqrt((sin(1/2*(ϕ2-ϕ1))**2)+cos(ϕ1)*cos(ϕ2)*(sin(1/2(λ2-λ1))**2)))
+print(d)
